f195400eda14e621f6c9aeda12d53638c5219b04
rachit <rachit.mangal@gmail.com>
2022-03-07 09:37:21 +0530

    logs added

--- startup/db.js
+++ startup/db.js
@@ -3,7 +3,12 @@ const winston = require("winston");
 const config = require("config");
 
 module.exports = function (app) {
-  mongoose.connect(config.mongodb_url).then((r) => {
+  // mongoose.connect(config.mongodb_url).then((r) => {
+  //   console.log("connected to mongodb");
+  //   winston.info("Connected to MongoDB");
+  // });
+
+  mongoose.connect(" mongodb://localhost/playground").then((r) => {
     console.log("connected to mongodb");
     winston.info("Connected to MongoDB");
   });
